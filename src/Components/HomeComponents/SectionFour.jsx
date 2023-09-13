dcde1e4bd72ad64aa645662d262e19683649483f
Udofa Daniel <danieludofa452@gmail.com>
2023-09-13 15:15:45 +0100

    travel packages

--- src/Components/HomeComponents/SectionFour.jsx
+++ src/Components/HomeComponents/SectionFour.jsx
@@ -4,16 +4,14 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-import imgOne from "../../Images/male-model.jpg";
-import imgTwo from "../../Images/model-one.jpg";
-import imgThree from "../../Images/male-model-2.jpg";
-import imgFour from "../../Images/model-two.jpg";
-import imgFive from "../../Images/male-model-3.jpg";
-import imgSix from "../../Images/model-three.jpg";
-import imgSeven from "../../Images/male-model-4.jpg";
-import imgEight from "../../Images/model-four.jpg";
-import imgNine  from "../../Images/male-model-5.jpg";
-import imgTen from "../../Images/model-five.jpg";
+import image1 from "../../Images/topproduct/image1.jpeg";
+import image2 from "../../Images/topproduct/image2.jpeg";
+import image3 from "../../Images/topproduct/image3.jpeg";
+import image4 from "../../Images/topproduct/image4.jpeg";
+import image5 from "../../Images/topproduct/image5.jpeg";
+import image6 from "../../Images/topproduct/image6.jpeg";
+import image7 from "../../Images/topproduct/image7.jpeg";
+import image8 from "../../Images/topproduct/image8.jpeg";
 
 export const SectionFour = () => {
   const breakpoints = {
@@ -52,9 +50,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgTwo}
+                src={image1}
                 alt="imgTwo"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -74,9 +72,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgOne}
+                src={image2}
                 alt="imgOne"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -97,9 +95,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgThree}
+                src={image3}
                 alt="imgThree"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -119,9 +117,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgFour}
+                src={image4}
                 alt="imgFour"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -141,9 +139,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgFive}
+                src={image5}
                 alt="imgFive"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -163,9 +161,9 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgSix}
+                src={image6}
                 alt="imgSix"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
+                className=" sec4ImgH w-full 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
               <div className="flex flex-col gap-4">
                 <div className="">
@@ -185,7 +183,7 @@ export const SectionFour = () => {
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgSeven}
+                src={image7}
                 alt="imgSeven"
                 className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
@@ -204,32 +202,11 @@ export const SectionFour = () => {
               </div>
             </div>
           </SwiperSlide>
+          
           <SwiperSlide>
             <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
               <img
-                src={imgEight}
-                alt="imgEight"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
-              />
-              <div className="flex flex-col gap-4">
-                <div className="">
-                  <h3 className="text-gray-900 font-semibold text-xl text-left md:text-lg">
-                    $295
-                  </h3>
-                  <p className="text-gray-500 font-semibold text-sm Aceh text-left md:font-medium">
-                    Hooded cotton sweatshirt
-                  </p>
-                </div>
-                <button className="w-full bg-gray-900 text-white font-medium text-base py-2 rounded-lg hover:bg-gray-800 transition duration-300 ease-in-out">
-                  Shop Now
-                </button>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
-              <img
-                src={imgNine}
+                src={image8}
                 alt="imgNine"
                 className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
               />
@@ -248,28 +225,7 @@ export const SectionFour = () => {
               </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="flex flex-col gap-3 w-max border-b border-gray-700 pb-2 sm:px-5 sm:w-full sm:border-none">
-              <img
-                src={imgTen}
-                alt="imgTen"
-                className=" sec4ImgH w-96 2xl:w-80 xl:w-72 md:w-60 sm:w-full"
-              />
-              <div className="flex flex-col gap-4">
-                <div className="">
-                  <h3 className="text-gray-900 font-semibold text-xl text-left md:text-lg">
-                    $295
-                  </h3>
-                  <p className="text-gray-500 font-semibold text-sm Aceh text-left md:font-medium">
-                    Hooded cotton sweatshirt
-                  </p>
-                </div>
-                <button className="w-full bg-gray-900 text-white font-medium text-base py-2 rounded-lg hover:bg-gray-800 transition duration-300 ease-in-out">
-                  Shop Now
-                </button>
-              </div>
-            </div>
-          </SwiperSlide>
+          
         </Swiper>
       </div>
     </div>
